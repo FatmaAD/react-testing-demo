bd8cd7291ac5466e3d2203c9ecf22392737b55a7
fatma <falaa@iotblue.net>
2022-06-19 13:44:04 +0200

    added baseurl

--- src/routs/index.tsx
+++ src/routs/index.tsx
@@ -4,14 +4,14 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const Landing = React.lazy(() => /*landing*/ import('../views/landing'));
 const Recipe = React.lazy(() => /*landing*/ import('../views/recipe'));
-
+const baseRoute = 'react-testing-demo'
 const AppRoutes = () => {
     return (
       <React.Suspense fallback={<CircularProgress />}>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/:recipeName" element={<Recipe />} />
+            <Route path={`${baseRoute}/`} element={<Landing />} />
+            <Route path={`${baseRoute}/:recipeName`} element={<Recipe />} />
           </Routes>
         </BrowserRouter>
       </React.Suspense>
